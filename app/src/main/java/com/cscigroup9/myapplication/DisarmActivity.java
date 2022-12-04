527ef44586a87900b557b85a72b4141aea7d5b57
DACarp <dane@carpenter.cx>
2022-12-03 20:29:25 -0500

    Basic layout, empty disarm activity.

---
+++ app/src/main/java/com/cscigroup9/myapplication/DisarmActivity.java
@@ -0,0 +1,14 @@
+package com.cscigroup9.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_disarm);
+    }
+}
