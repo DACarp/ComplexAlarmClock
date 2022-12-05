be0d0dc896686092a3b4ffe5e744d024bc5e13fe
DACarp <dane@carpenter.cx>
2022-12-05 11:46:06 -0500

    Basic layout, empty disarm activity.

--- app/src/main/java/com/cscigroup9/myapplication/MainActivity.java
+++ app/src/main/java/com/cscigroup9/myapplication/MainActivity.java
@@ -6,9 +6,23 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    private boolean algebraTrue;
+    private int numTasks;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        algebraTrue = true;
+        numTasks = 5;
+
     }
+
+
+
+
+
 }
